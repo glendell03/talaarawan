3d569c8ca0ed89ccf659065d3a1beeeceaeb902a
jinnielqtqt <jibbautista@iskolarngbayan.pup.edu.ph>
2021-02-17 21:46:37 +0800

    login auth

---
+++ server/login.php
@@ -0,0 +1,70 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("location: index.php");
+        exit;
+    }
+     require_once "config.php";
+
+     $email = $password = "";
+     $email_err = $password_err = "";
+
+     if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+        if(empty(trim($_POST["email"]))){
+            $email_err = "Please enter email.";
+        } else{
+            $email = trim($_POST["email"]);
+        }
+        
+        if(empty(trim($_POST["password"]))){
+            $password_err = "Please enter your password.";
+        } else{
+            $password = trim($_POST["password"]);
+        }
+
+        if(empty($email_err) && empty($password_err)){
+            $sql = "SELECT id, email, password FROM users WHERE email = ?";
+
+            if($stmt = mysqli_prepare($link, $sql)){
+                mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+                $param_email = $email;
+                if(mysqli_stmt_execute($stmt)){
+                    mysqli_stmt_store_result($stmt);
+
+                    if(mysqli_stmt_num_rows($stmt) == 1){
+                        mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password);   
+                        
+                        if(mysqli_stmt_fetch($stmt)){
+                            if(password_verify($password, $hashed_password)){
+                               
+                            session_start();
+                            
+                            
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["email"] = $email;                            
+                            
+                            
+                            header("location: index.php");
+                        } else{
+                            
+                            $password_err = "The password you entered was not valid.";
+                        }  
+                    }  
+                }else{
+                        
+                        $email_err = "No account found with that email.";
+                }
+
+            }else{
+                    echo "Oops! Something went wrong. Please try again later.";
+            }
+                mysqli_stmt_close($stmt);
+            }
+        }
+    mysqli_close($link);
+}
+?>
