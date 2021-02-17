a6d3f3ff29f61101a520fd59805bde9aca5e964d
glendell03 <dellbringino@gmail.com>
2021-02-17 17:02:03 +0800

    Add search ui

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@
     <title>Talaarawan</title>
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/note.css">
+    <link rel="stylesheet" href="./css/search.css">
 </head>
 
 <body>
@@ -23,6 +24,37 @@
             <button type="submit">submit</button>
         </form>
     </div> -->
+
+    <!-- SEARCH -->
+    <div class="searchbar">
+
+        <input type="search" name="search" id="search" placeholder="Search notes">
+
+        <div class="select-wrapper">
+            <div class="vLine"></div>
+            <select name="month" id="month">
+                <option value="Month">MM</option>
+                <option name="January" value="Jan">January</option>
+                <option name="February" value="Feb">February</option>
+                <option name="March" value="Mar">March</option>
+                <option name="April" value="Apr">April</option>
+                <option name="May" value="May">May</option>
+                <option name="June" value="Jun">June</option>
+                <option name="July" value="Jul">July</option>
+                <option name="August" value="Aug">August</option>
+                <option name="September" value="Sep">September</option>
+                <option name="October" value="Oct">October</option>
+                <option name="November" value="Nov">November</option>
+                <option name="December" value="Dec">December</option>
+            </select>
+            <div class="vLine"></div>
+            <select name="year" id="year">
+                <option>YY</option>
+                <option value="2020">2021</option>
+                <option value="2020">2020</option>
+            </select>
+        </div>
+    </div>
 
     <!-- NOTE -->
     <div class="note-wrapper">
