cd84a937ee485b8ada935105ce5cbb30cd82b55a
ravenlntn <rgdlantin@iskolarngbayan.pup.edu.ph>
2021-02-23 15:16:29 +0800

    icon update

--- public/pages/update.php
+++ public/pages/update.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo_1.png">
     <title>Update</title>
     <link rel="stylesheet" href="../css/update.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
